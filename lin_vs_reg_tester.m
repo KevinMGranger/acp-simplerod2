e7de7f763b82a6fd666c08c5482b8d0d1b662a8b
Kevin Granger <kmg2728@rit.edu>
2013-01-17 13:08:24 -0500

    PARALLEL IS THAT EASY. SEIOUSLY.

--- lin_vs_reg_tester.m
+++ lin_vs_reg_tester.m
@@ -1,18 +1,20 @@
 function [nums lin_times reg_times] = lin_vs_reg_tester
 
-nums = round(logspace(1,5,20));
+nums = round(logspace(1,5,10));
 lin_times = zeros(1,length(nums));
 reg_times = lin_times;
 
 
-for i=1:length(nums)
-    fprintf('%u Pieces\n',nums(i));
-    
+parfor i=1:length(nums)
+    fprintf('Starting lin # %u for %u pieces',i,nums(i));
     tic;
     lin_simple_rod(nums(i),100,10);
     lin_times(i) = toc;
     fprintf('Lin time # %u for %u pieces : %u seconds\n',i,nums(i),lin_times(i));
-    
+end
+
+parfor i=1:length(nums)
+    fprintf('Starting reg # %u for %u pieces',i,nums(i));
     tic;
     simple_rod(nums(i),100,10);
     reg_times(i) = toc;
